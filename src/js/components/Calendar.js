99fc7d3627a5a3301dfc561e33002f274570baa7
Will Doenlen <wdoenlen@gmail.com>
2021-05-23 23:41:24 -0400

    Flexible number of rows for calendar

--- src/js/components/Calendar.js
+++ src/js/components/Calendar.js
@@ -1,103 +1,74 @@
 import React from "react"
 
-export default ({ isVisible }) => {
+const Calendar = ({ isVisible }) => {
   return (
     <div
-      class="grid"
       style={{
         position: "fixed",
         width: "100%",
         height: "100%",
         backgroundColor: "white",
         zIndex: 1000,
-        display: isVisible ? "grid" : "none",
+        display: isVisible ? "flex" : "none",
       }}
     >
-      <div class="cell header">
-        <h1 class="day">周日</h1>
-      </div>
-      <div class="cell header">
-        <h1 class="day">周一</h1>
-      </div>
-      <div class="cell header">
-        <h1 class="day">周二</h1>
-      </div>
-      <div class="cell header">
-        <h1 class="day">周三</h1>
-      </div>
-      <div class="cell header">
-        <h1 class="day">周四</h1>
-      </div>
-      <div class="cell header">
-        <h1 class="day">周五</h1>
-      </div>
-      <div class="cell header">
-        <h1 class="day">周六</h1>
-      </div>
-      {/* <!-- Row --> */}
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      {/* <!-- Row --> */}
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      {/* <!-- Row --> */}
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      {/* <!-- Row --> */}
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      {/* <!-- Row --> */}
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      {/* <!-- Row --> */}
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      {/* <!-- Row --> */}
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      {/* <!-- Row --> */}
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
+      {/* <Sidebar /> */}
+      <Grid numRows={12} />
     </div>
   )
 }
+
+const Sidebar = () => {
+  return (
+    <div
+      style={{
+        height: "100%",
+        width: "300px",
+      }}
+    ></div>
+  )
+}
+
+const Grid = ({ numRows }) => {
+  const createRows = () => {
+    return Array.from(Array(numRows)).flatMap(() => {
+      return [
+        <div className="cell"></div>,
+        <div className="cell"></div>,
+        <div className="cell"></div>,
+        <div className="cell"></div>,
+        <div className="cell"></div>,
+        <div className="cell"></div>,
+        <div className="cell"></div>,
+      ]
+    })
+  }
+  return (
+    <div className="grid">
+      <div className="cell header">
+        <h1 className="day">周日</h1>
+      </div>
+      <div className="cell header">
+        <h1 className="day">周一</h1>
+      </div>
+      <div className="cell header">
+        <h1 className="day">周二</h1>
+      </div>
+      <div className="cell header">
+        <h1 className="day">周三</h1>
+      </div>
+      <div className="cell header">
+        <h1 className="day">周四</h1>
+      </div>
+      <div className="cell header">
+        <h1 className="day">周五</h1>
+      </div>
+      <div className="cell header">
+        <h1 className="day">周六</h1>
+      </div>
+      {createRows()}
+    </div>
+  )
+}
+
+export default Calendar
